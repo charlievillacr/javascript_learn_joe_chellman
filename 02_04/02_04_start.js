444da3cfbba547b7509b871e8b843ece3eb4e1ac
charlie villalobos <charlievilla@gmail.com>
2018-03-05 11:44:53 -0600

    Booleans

---
+++ 02_04/02_04_start.js
@@ -0,0 +1,18 @@
+true
+true
+false
+false
+True
+VM78:1 Uncaught ReferenceError: True is not defined
+    at <anonymous>:1:1
+False
+VM97:1 Uncaught ReferenceError: False is not defined
+    at <anonymous>:1:1
+
+var myLocation = "Graz";
+    undefined
+myOtherLocation = "Los Angeles";
+    "Los Angeles"
+myLocation === myOtherLocation
+    false
+​
