ff43f0eb873681ce77de7b26d70adc209232e74e
charlie villalobos <charlievilla@gmail.com>
2018-03-05 11:10:09 -0600

    Started Course

---
+++ 02_01/02_01.js
@@ -0,0 +1,24 @@
+// This is a transcript of the lines of code from the lesson.
+// You can copy and paste each line into a JavaScript console to execute it and see the result.
+
+var x = 32;
+x;
+
+var whereAmI = "Graz, Austria";
+whereAmI;
+
+x = 45;
+x;
+whereAmI = 75;
+whereAmI;
+
+var monster1 = "Grover", monster2 = 'Cookie Monster', monster3 = 'Animal';
+monster1;
+monster2;
+monster3;
+
+var presidentOfUSA = "Obama"; // as of late summer 2012
+presidentOfUSA;
+
+// More info:
+// https://developer.mozilla.org/en-US/docs/JavaScript/Reference/Statements/var
